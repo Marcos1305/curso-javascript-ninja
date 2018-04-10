eb09039f595bfff179c4f3927ccff24b09a0d1e9
Marcos1305 <marcos1305pt@gmail.com>
2018-04-10 15:20:56 -0300

    Challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var newarray = ['marcos' , 'vinicius', 13, 05, 'Masculino'];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function printArray(param){
+  return param;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(printArray(newarray)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +25,25 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function myFunction(param1, param2){
+    return param1[param2];
+};
+
+console.log(myFunction(newarray, 3));
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arrayall = ['marcos', 13, true, null, undefined,]
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+ printArray(arrayall);
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +59,59 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(namebook){
+  var livro  = namebook;
+  var objbook = {
+      'A revolução dos Bichos': {
+      quantidadePaginas : 152,
+      autor: 'Orwell, George',
+      editora: 'Companhia Das letras'
+    },
+    'Admirável Mundo Novo': {
+      quantidadePaginas : 314,
+      autor: 'Huxley, Aldous',
+      editora: 'Biblioteca Azul'
+    },
+    'Senhor Das Moscas': {
+      quantidadePaginas: 200,
+      autor: 'Golding, William',
+      editora: 'Alfaguara, Objetiva'
+    }
+  };
+
+  if (namebook === undefined){
+    return objbook;
+  }
+
+  return objbook[namebook];
+
+};
+
+
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+console.log('O livro Senhor das Moscas tem ' +book('Senhor Das Moscas').quantidadePaginas+ ' páginas!' );
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log('O auto do livro A revolução dos Bichos é ' + book('A revolução dos Bichos').autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log('O livro Admirável Mundo Novo foi publicado pela editora ' + book('Admirável Mundo Novo').editora + '.');
